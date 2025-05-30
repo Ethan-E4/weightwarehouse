f99c06e77719f7261755ebe55b99ea5cd427c0aa
Ethan-E4 <ethanespineli04@gmail.com>
2025-05-30 18:15:14 -0500

    Created about page

---
+++ about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda et maiores esse libero, autem labore numquam accusantium corrupti itaque similique non cumque, dicta in beatae magni quam, magnam nisi. Laudantium soluta ipsam magni error, illum vitae quae eum eligendi praesentium architecto asperiores, perferendis, optio accusantium nisi? Eos ea tempora atque?</p>
+</body>
+</html>
